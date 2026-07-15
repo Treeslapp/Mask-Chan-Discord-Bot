69b3b2b2b1f57280def80e14b265ed99c663f885
Tree Slapper <organizationden@gmail.com>
2026-07-15 11:05:26 -0400

    Comma added to flirts.py

--- flirts.py
+++ flirts.py
@@ -5,7 +5,7 @@ flirts = ("Ew, even my standards aren't *that* low.",
          ":3 hiiii~", 
          "Your skin looks very moist today o_o", 
          "You know you can't hide from me :p", 
-         "Look. I need to talk to you about something important. We've known each other for a very long time, and I think I'm beginning to develop feelings. I'm glad we were friends, but I can't be your friend for any longer. Would you like to begin building a relationship with me? If not, I understand - I just wanted to be honest about my feelings."
+         "Look. I need to talk to you about something important. We've known each other for a very long time, and I think I'm beginning to develop feelings. I'm glad we were friends, but I can't be your friend for any longer. Would you like to begin building a relationship with me? If not, I understand - I just wanted to be honest about my feelings.",
          "Hey there, are you from Tennessee? Because you have a very thick Tennessian accent.",
          "Did it hurt when you fell? Because there's a very large crater in the ground.",
          "Hewwo pwincess. :detective:"
